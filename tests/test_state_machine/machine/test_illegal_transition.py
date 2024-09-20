1915bfb7eb6f1ae3320859c81a7bfdd617c51c8e
Danny McRae <bobdachs@gmail.com>
2024-09-20 05:48:15 -0400

    Update test_illegal_transition.py

---
+++ tests/test_state_machine/machine/test_illegal_transition.py
@@ -0,0 +1,91 @@
+# third party imports
+import pytest
+
+# application imports
+from state_machine.exception.machine import IllegalTransitionError
+import state_machine
+from state_machine import BaseState, Transition
+from state_machine.decorator import machine, node, no_exceptions
+
+# mock imports
+from tests.mocks import MockLogger
+
+
+def test_illegal_transition():
+    with pytest.raises(IllegalTransitionError):
+
+        @machine
+        class Machine(state_machine.AbstractMachine):
+            """
+            overview:
+                Yo.
+            """
+
+            @no_exceptions
+            @node
+            def entry(self) -> Transition:
+                """
+                overview:
+                    Yo.
+
+                is_entry: True
+
+                happy_paths:
+                    - happy
+                unhappy_paths:
+                    - unhappy
+                """
+                return self.success(exit_to=self.happier)
+
+            @no_exceptions
+            @node
+            def happy(self) -> Transition:
+                """
+                overview:
+                    Yo.
+
+                happy_paths:
+                    - happier
+                unhappy_paths:
+                    - unhappy
+                """
+                return self.failure(exit_to=self.unhappy, message="uh oh")
+
+            @no_exceptions
+            @node
+            def happier(self) -> Transition:
+                """
+                overview:
+                    Yo.
+
+                is_terminal: True
+                """
+                return self.exit()
+
+            @no_exceptions
+            @node
+            def unhappy(self) -> Transition:
+                """
+                overview:
+                    Yo.
+
+                happy_paths:
+                    - report_results
+                """
+                return self.success(exit_to=self.report_results)
+
+            @property
+            def failure_prefix(self) -> str:
+                return "Machine"
+
+            @property
+            def state(self) -> BaseState:
+                return self._state
+
+        logger = MockLogger()
+
+        test_machine = Machine(
+            logger=logger,
+            state=BaseState(),
+        )
+        test_machine.execute()
