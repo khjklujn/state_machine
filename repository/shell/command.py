6c0701ed6fe877d991f54ca4ac3de30f6534301c
Danny McRae <bobdachs@gmail.com>
2024-09-20 09:12:00 -0400

    Update command.py

---
+++ repository/shell/command.py
@@ -0,0 +1,56 @@
+# standard library imports
+from datetime import datetime
+from os import environ, _Environ
+import subprocess
+from typing import Optional
+
+
+# repository imports
+from state_machine import AbstractRepository
+from long_term_storage.repository.shell.delimited import SpaceDelimited
+
+
+class Command(AbstractRepository):
+    """
+    Base class for executing command line actions.
+    """
+
+    @classmethod
+    def execute(
+        cls,
+        *,
+        command: SpaceDelimited,
+        cwd: Optional[str] = None,
+        env: _Environ = environ,
+        start_new_session: bool = False,
+        input: Optional[str] = None,
+    ) -> subprocess.CompletedProcess[str]:
+        """
+        Executes the command line action.
+
+        raises:
+            Exception: If exit code is not zero.
+        """
+        start_time = datetime.utcnow()
+        cls.logger.debug(f"  {command} - Started")
+
+        result = subprocess.run(
+            command.get_secret_value(),
+            capture_output=True,
+            env=env,
+            cwd=cwd,
+            text=True,
+            start_new_session=start_new_session,
+            input=input,
+        )
+
+        end_time = datetime.utcnow()
+        if result.returncode != 0:
+            cls.logger.debug(
+                f"  {command} - Error: {result.returncode} - Runtime: {end_time - start_time}"
+            )
+            raise Exception(result.stderr)
+
+        cls.logger.debug(f"  {command} - Completed - Runtime: {end_time - start_time}")
+
+        return result
